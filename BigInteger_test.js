8094d5180ac59920e3a318368e4813f62c998420
Ramon Boss <ramon.bossooutlook.com>
2017-07-10 12:35:00 +0200

    added test to addition

--- BigInteger_test.js
+++ BigInteger_test.js
@@ -46,9 +46,7 @@ describe('BigInteger', function () {
   it('add', function () {
     expect(ZERO.add(ZERO)).to.deep.equal(ZERO)
     expect(ZERO.add(ONE)).to.deep.equal(ONE)
-
-    // expect(BigInteger.from([0, 0, 0, 0, 0, 0, 0, 0, 1, 0]).isOne()).to.equal(false)
-    // expect(BigInteger.from([1, 2, 3, 4, 5, 6, 7, 8, 9]).isOne()).to.equal(false)
+    expect(BIG_NUM_1.add(BIG_NUM_2)).to.deep.equal(BigInteger.from([0x80, 0x7A, 0xCB, 0x5C, 0x38, 0xAC, 0x6D, 0xAD, 0xBF]))
   })
 
   it('and', function () {
